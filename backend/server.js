404892862269a8211e125d9cfdb0605a4d267353
Kartikey Gupta <55681108+kartikey19427@users.noreply.github.com>
2024-11-29 07:47:01 +0000

    router started

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
-import express from 'express';
+import express from "express";
 
 
 console.log("server is running!");
 const app=express();
-app.listen(req, res)
+app.get("/",(req, res)=>{
+    res.send("server is ready!");
+});
+
+app.listen(5000, () => {
+    console.log("working as intented");
+});
